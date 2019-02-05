cbf164ac6f9a130be04be497fda72225109e58ca
OmerBenGera <omerthepro126@gmail.com>
2019-02-05 20:07:53 +0200

    Fixed exception with newer versions of Essentials

--- src/main/java/xyz/wildseries/wildchests/hooks/PricesProvider_Essentials.java
+++ src/main/java/xyz/wildseries/wildchests/hooks/PricesProvider_Essentials.java
@@ -1,6 +1,8 @@
 package xyz.wildseries.wildchests.hooks;
 
 import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.IEssentials;
+import com.earth2me.essentials.Worth;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import xyz.wildseries.wildchests.WildChestsPlugin;
@@ -15,7 +17,17 @@ public final class PricesProvider_Essentials implements PricesProvider {
 
     @Override
     public double getPrice(Player player, ItemStack itemStack) {
-        BigDecimal price = Essentials.getPlugin(Essentials.class).getWorth().getPrice(itemStack);
+        Essentials plugin = Essentials.getPlugin(Essentials.class);
+        Worth worth = plugin.getWorth();
+        BigDecimal price = null;
+        try {
+            price = worth.getPrice(itemStack);
+        }catch(Throwable ex){
+            try {
+                price = (BigDecimal) worth.getClass().getMethod("getPrice", IEssentials.class, ItemStack.class)
+                        .invoke(worth, plugin, itemStack);
+            }catch(Exception ignored){}
+        }
         return price == null ? -1 : price.doubleValue() * itemStack.getAmount();
     }
 }
