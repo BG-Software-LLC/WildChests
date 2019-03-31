4cc89d1b0287cbdbd440d1b12509397514c3ceb3
OmerBenGera <omerthepro126@gmail.com>
2019-03-31 23:49:24 +0300

    Limited titles to 32 chars long

--- src/main/java/com/bgsoftware/wildchests/objects/WInventory.java
+++ src/main/java/com/bgsoftware/wildchests/objects/WInventory.java
@@ -26,6 +26,8 @@ public final class WInventory{
     }
 
     public void setTitle(String title){
+        if(title.length() > 32)
+            title = title.substring(0, 32);
         try {
             Class minecraftInventory = Arrays.stream(this.inventory.getClass().getDeclaredClasses())
                     .filter(clazz -> clazz.getName().contains("MinecraftInventory")).findFirst().get();
