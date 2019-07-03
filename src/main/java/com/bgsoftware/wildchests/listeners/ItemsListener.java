df104cc19aec611570eab1be8518e987214ceaea
OmerBenGera <omerthepro126@gmail.com>
2019-07-03 22:25:55 +0300

    Fixed some delay issues

--- src/main/java/com/bgsoftware/wildchests/listeners/ItemsListener.java
+++ src/main/java/com/bgsoftware/wildchests/listeners/ItemsListener.java
@@ -3,12 +3,14 @@ package com.bgsoftware.wildchests.listeners;
 import com.bgsoftware.wildchests.WildChestsPlugin;
 import com.bgsoftware.wildchests.api.objects.chests.Chest;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
 
 import java.util.List;
 
+@SuppressWarnings({"unused", "WeakerAccess"})
 public final class ItemsListener implements Listener {
 
     private WildChestsPlugin plugin;
@@ -20,14 +22,24 @@ public final class ItemsListener implements Listener {
     @EventHandler
     public void onItemSpawn(ItemSpawnEvent e){
         Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, () -> {
-            List<Chest> chestList = plugin.getChestsManager().getNearbyChests(e.getLocation());
-            for(Chest chest : chestList){
-                if(chest.addItems(e.getEntity().getItemStack()).isEmpty()){
-                    e.getEntity().remove();
-                    break;
+            if(e.getEntity().isValid() && !e.getEntity().isDead()) {
+                if(!e.getEntity().isOnGround()) {
+                    onItemSpawn(e);
+                    return;
                 }
+                trySuction(e.getEntity());
             }
         }, 20L);
+    }
+
+    private void trySuction(Item item){
+        List<Chest> chestList = plugin.getChestsManager().getNearbyChests(item.getLocation());
+        for (Chest chest : chestList) {
+            if (chest.addItems(item.getItemStack()).isEmpty()) {
+                item.remove();
+                break;
+            }
+        }
     }
 
 }
