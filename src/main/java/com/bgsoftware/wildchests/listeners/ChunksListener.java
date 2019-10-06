942602199ffb310d95c6aa4359adeee72acb3c45
OmerBenGera <omerthepro126@gmail.com>
2019-10-06 21:18:17 +0300

    Fixed an issue with hoppers not being registered

--- src/main/java/com/bgsoftware/wildchests/listeners/ChunksListener.java
+++ src/main/java/com/bgsoftware/wildchests/listeners/ChunksListener.java
@@ -2,11 +2,16 @@ package com.bgsoftware.wildchests.listeners;
 
 import com.bgsoftware.wildchests.WildChestsPlugin;
 import com.bgsoftware.wildchests.api.objects.chests.Chest;
+import com.bgsoftware.wildchests.task.HopperTask;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.Hopper;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 
 public final class ChunksListener implements Listener {
 
@@ -21,8 +26,23 @@ public final class ChunksListener implements Listener {
         for(BlockState blockState : e.getChunk().getTileEntities()){
             if(blockState instanceof org.bukkit.block.Chest){
                 Chest chest = plugin.getChestsManager().getChest(blockState.getLocation());
-                if(chest != null)
+                if(chest != null) {
                     plugin.getNMSAdapter().updateTileEntity(chest);
+                    Block hopperBlock = blockState.getBlock().getRelative(BlockFace.DOWN);
+                    if(hopperBlock.getState() instanceof Hopper)
+                        HopperTask.addHopper(chest, (Hopper) hopperBlock.getState());
+                }
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onChunkUnload(ChunkUnloadEvent e){
+        for(BlockState blockState : e.getChunk().getTileEntities()){
+            if(blockState instanceof org.bukkit.block.Chest){
+                Chest chest = plugin.getChestsManager().getChest(blockState.getLocation());
+                if(chest != null)
+                    HopperTask.removeHopper(chest);
             }
         }
     }
