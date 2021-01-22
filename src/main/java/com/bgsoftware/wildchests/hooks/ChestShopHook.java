a9da75ba261a5e559bb4c0634d95bb5ebbb137bd
OmerBenGera <omerthepro126@gmail.com>
2021-01-22 22:03:10 +0200

    Fixed the ability to create shops out of wild chests

--- src/main/java/com/bgsoftware/wildchests/hooks/ChestShopHook.java
+++ src/main/java/com/bgsoftware/wildchests/hooks/ChestShopHook.java
@@ -1,14 +1,15 @@
 package com.bgsoftware.wildchests.hooks;
 
 import com.Acrobot.ChestShop.Events.PreShopCreationEvent;
-import com.Acrobot.ChestShop.Events.PreTransactionEvent;
 import com.bgsoftware.wildchests.WildChestsPlugin;
 import com.bgsoftware.wildchests.api.objects.chests.Chest;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
-import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.type.Sign;
+import org.bukkit.block.data.type.WallSign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -33,15 +34,14 @@ public final class ChestShopHook {
                 public void onChestShopCreate(PreShopCreationEvent e) {
                     try {
                         BlockData blockData = (BlockData) GET_BLOCK_DATA.invoke(e.getSign());
-                        if(blockData instanceof Directional){
-                            Block attachedBlock = e.getSign().getBlock().getRelative(
-                                    ((Directional) blockData).getFacing().getOppositeFace());
-
-                            Chest chest = plugin.getChestsManager().getChest(attachedBlock.getLocation());
-
-                            if(chest != null) {
-                                e.setCancelled(true);
-                                e.setSignLines(new String[] {"", "", "", ""});
+                        if(blockData instanceof WallSign || blockData instanceof Sign){
+                            for(Location location : getPotentialChests(e.getSign().getBlock())){
+                                Chest chest = plugin.getChestsManager().getChest(location);
+                                if(chest != null) {
+                                    e.setCancelled(true);
+                                    e.setSignLines(new String[] {"", "", "", ""});
+                                    break;
+                                }
                             }
                         }
                     }catch (Exception ex){
@@ -51,6 +51,17 @@ public final class ChestShopHook {
 
             }, plugin);
         }
+    }
+
+    private static Location[] getPotentialChests(Block block){
+        return new Location[] {
+                block.getLocation().add(1, 0, 0),
+                block.getLocation().add(0, 1, 0),
+                block.getLocation().add(0, 0, 1),
+                block.getLocation().add(-1, 0, 0),
+                block.getLocation().add(0, -1, 0),
+                block.getLocation().add(0, 0, -1)
+        };
     }
 
 }
