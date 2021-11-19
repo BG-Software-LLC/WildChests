81cbf3b1e15961dc09c4d39799da24ec990f069f
OmerBenGera <omerthepro126@gmail.com>
2021-11-19 14:20:27 +0200

    Fix usage of doubles when formatting BigDecimals

--- src/main/java/com/bgsoftware/wildchests/utils/StringUtils.java
+++ src/main/java/com/bgsoftware/wildchests/utils/StringUtils.java
@@ -1,38 +1,37 @@
 package com.bgsoftware.wildchests.utils;
 
-import com.bgsoftware.wildchests.Locale;
-
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public final class StringUtils {
 
-    private static DecimalFormat numberFormatter = new DecimalFormat("###,###,###,###,###,###,###,###,###,##0.00");
-    private static final long Q = 1_000_000_000_000_000L, T = 1_000_000_000_000L, B = 1_000_000_000L, M = 1_000_000L, K = 1_000L;
+    private static final DecimalFormat numberFormatter = new DecimalFormat("###,###,###,###,###,###,###,###,###,##0.00");
+    private static final BigDecimal K = BigDecimal.valueOf(1000), M = K.multiply(K), B = M.multiply(K),
+            T = B.multiply(K), Q = T.multiply(K);
 
-    public static String format(BigDecimal bigDecimal){
+    public static String format(BigDecimal bigDecimal) {
         String s = numberFormatter.format(Double.parseDouble(bigDecimal.toString()));
         return s.endsWith(".00") ? s.replace(".00", "") : s;
     }
 
-    public static String format(double d)
-    {
-        return format(BigDecimal.valueOf(d));
+    public static String fancyFormat(BigDecimal bigDecimal) {
+        if (bigDecimal.compareTo(Q) >= 0)
+            return format(bigDecimal.divide(Q, 2, RoundingMode.HALF_UP));
+
+        if (bigDecimal.compareTo(T) >= 0)
+            return format(bigDecimal.divide(T, 2, RoundingMode.HALF_UP));
+
+        if (bigDecimal.compareTo(B) >= 0)
+            return format(bigDecimal.divide(B, 2, RoundingMode.HALF_UP));
+
+        if (bigDecimal.compareTo(M) >= 0)
+            return format(bigDecimal.divide(M, 2, RoundingMode.HALF_UP));
+
+        if (bigDecimal.compareTo(K) >= 0)
+            return format(bigDecimal.divide(K, 2, RoundingMode.HALF_UP));
+
+        return format(bigDecimal);
     }
 
-    public static String fancyFormat(BigDecimal bigDecimal){
-        double d = bigDecimal.longValue();
-        if(d >= Q)
-            return format((d / Q)) + Locale.FORMAT_QUAD.getMessage();
-        else if(d >= T)
-            return format((d / T)) + Locale.FORMAT_TRILLION.getMessage();
-        else if(d >= B)
-            return format((d / B)) + Locale.FORMAT_BILLION.getMessage();
-        else if(d >= M)
-            return format((d / M)) + Locale.FORMAT_MILLION.getMessage();
-        else if(d >= K)
-            return format((d / K)) + Locale.FORMAT_THOUSANDS.getMessage();
-        else
-            return format(d);
-    }
 }
