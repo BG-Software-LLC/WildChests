abfccb827b7828e0cf1467e24b03629af52a79b2
OmerBenGera <omerthepro126@gmail.com>
2022-08-03 15:10:25 +0300

    Added nms utilities to better detection incorrect usage of nms

---
+++ src/main/java/com/bgsoftware/wildchests/nms/mapping/RemapFailure.java
@@ -0,0 +1,9 @@
+package com.bgsoftware.wildchests.nms.mapping;
+
+public class RemapFailure extends RuntimeException {
+
+    public RemapFailure(String message) {
+        super(message);
+    }
+
+}
