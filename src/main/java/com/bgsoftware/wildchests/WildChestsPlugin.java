8a31a6766d82947f879f5989cd52bfd42171815f
OmerBenGera <omerthepro126@gmail.com>
2022-09-03 09:31:05 +0300

    Changed it so when failing to retrieve mappings for the server the plugin doesn't fail to load

--- src/main/java/com/bgsoftware/wildchests/WildChestsPlugin.java
+++ src/main/java/com/bgsoftware/wildchests/WildChestsPlugin.java
@@ -154,7 +154,10 @@ public final class WildChestsPlugin extends JavaPlugin implements WildChests {
 
             String mappingVersionHash = nmsAdapter.getMappingsHash();
 
-            if (mappingVersionHash != null && !MappingsChecker.checkMappings(mappingVersionHash, version)) {
+            if (mappingVersionHash != null && !MappingsChecker.checkMappings(mappingVersionHash, version, error -> {
+                log("&cFailed to retrieve allowed mappings for your server, skipping...");
+                return true;
+            })) {
                 log("Error while loading adapter - your version mappings are not supported... Please contact @Ome_R");
                 log("Your mappings version: " + mappingVersionHash);
                 return false;
