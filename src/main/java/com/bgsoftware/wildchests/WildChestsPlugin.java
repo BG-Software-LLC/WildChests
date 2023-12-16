756d070d737c4720705128cb4ede8cce68cd012e
OmerBenGera <omerthepro126@gmail.com>
2023-12-16 17:21:03 +0200

    Adds support to 1.20.4

--- src/main/java/com/bgsoftware/wildchests/WildChestsPlugin.java
+++ src/main/java/com/bgsoftware/wildchests/WildChestsPlugin.java
@@ -170,7 +170,8 @@ public final class WildChestsPlugin extends JavaPlugin implements WildChests {
                     new Pair<>(3336, null),
                     new Pair<>(3337, "v1_19"),
                     new Pair<>(3465, "v1_20_1"),
-                    new Pair<>(3578, "v1_20_2")
+                    new Pair<>(3578, "v1_20_2"),
+                    new Pair<>(3700, "v1_20_3")
             );
 
             for (Pair<Integer, String> versionData : versions) {
