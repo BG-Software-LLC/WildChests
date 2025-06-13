6d14d82e185e1c7d568a71517ebae9f96ae9b6a4
OmerBenGera <omerthepro126@gmail.com>
2025-06-13 18:07:57 +0300

    Do not use ServerLevel#sendParticlesSource which does not exist in Spigot

--- NMS/v1_21_4/src/main/java/com/bgsoftware/wildchests/nms/v1_21_4/inventory/WildChestBlockEntity.java
+++ NMS/v1_21_4/src/main/java/com/bgsoftware/wildchests/nms/v1_21_4/inventory/WildChestBlockEntity.java
@@ -188,7 +188,7 @@ public class WildChestBlockEntity extends ChestBlockEntity implements WorldlyCon
             double z = blockPos.getZ() + level.getRandom().nextFloat();
             for (String particle : chestData.getChestParticles()) {
                 try {
-                    this.serverLevel.sendParticlesSource(null,
+                    this.serverLevel.sendParticles(
                             CraftParticle.createParticleParam(Particle.valueOf(particle), null),
                             false, false, x, y, z, 0, 0.0, 0.0, 0.0, 1.0);
                 } catch (Exception ignored) {
@@ -335,8 +335,7 @@ public class WildChestBlockEntity extends ChestBlockEntity implements WorldlyCon
     }
 
     private void handleItemSuctionRemoval(ItemEntity itemEntity) {
-        this.serverLevel.sendParticlesSource(null,
-                CraftParticle.createParticleParam(Particle.CLOUD, null),
+        this.serverLevel.sendParticles(CraftParticle.createParticleParam(Particle.CLOUD, null),
                 false, false, itemEntity.getX(), itemEntity.getY(), itemEntity.getZ(),
                 0, 0.0, 0.0, 0.0, 1.0);
         itemEntity.discard();
